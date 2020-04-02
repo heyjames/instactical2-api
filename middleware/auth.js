fef0207d0995dba94aedeb058807084ca490c8f2
PC\James <heyjameslam@gmail.com>
2020-04-02 00:13:36 -0700

    Minor auth change. Allow access to custom header.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-module.exports = function auth(req, res, next) {
+module.exports = function (req, res, next) {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied. No token provided.");
 
   try {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
-    req.user = decoded;
+    req.user = decoded; // You can now access user._id. See user.js model func generateAuthToken()
     next();
   }
   catch (ex) {
--- routes/users.js
+++ routes/users.js
@@ -35,7 +35,7 @@ router.post("/", async (req, res) => {
 
   const token = user.generateAuthToken();
 
-  res.header("x-auth-token", token).send(_.pick(user, ["_id", "name", "email"]));
+  res.header("x-auth-token", token).header("access-control-expose-headers", "x-auth-token").send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
