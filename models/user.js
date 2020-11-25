ab2e398dd8155fe134f0837c67d68eb3b62de844
James <>
2020-11-24 22:05:57 -0800

    Change login validation rules

--- models/user.js
+++ models/user.js
@@ -14,14 +14,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 5,
-    maxlength: 255,
+    maxlength: 50,
     unique: true
   },
   password: {
     type: String,
     required: true,
     minlength: 5,
-    maxlength: 1024
+    maxlength: 50
   },
   isAdmin: Boolean
 });
@@ -41,8 +41,8 @@ const User = mongoose.model('User', userSchema);
 function validateUser(user) {
   const schema = {
     name: Joi.string().min(5).max(50).required(),
-    email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(255).required()
+    email: Joi.string().min(5).max(50).required().email(),
+    password: Joi.string().min(5).max(50).required()
   };
 
   return Joi.validate(user, schema);
